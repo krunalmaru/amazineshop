b7d01a677fdaefcae8b53683eef4337f733b8ea7
krunal <krunal.maru1106@gmail.com>
2023-01-23 17:09:37 +0530

    add image logo and change background

--- newecom/authuse/views.py
+++ newecom/authuse/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+        if password != confirm_password:
+            messages.warning(request, "Password did't Match")
+            return render(request,'signup.html')
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse("User Already Exist With This Email")
+        except Exception as identifier:
+            pass
+        user = User.objects.create_user(email,email,password)    
+        user.save()
+        return HttpResponse("user created")
     return render(request, 'signup.html')
 
 def loginview(request):
--- newecom/newecom/urls.py
+++ newecom/newecom/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecomapp.urls')),
-    path('auth/', include('authuse.urls'))
+    path('authuse/', include('authuse.urls'))
 ]
