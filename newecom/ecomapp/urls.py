ee98ab6b1ff959bd24b49d75c18e076a90fd42b6
krunal <krunal.maru1106@gmail.com>
2023-01-23 12:07:42 +0530

    create home page

--- newecom/ecomapp/urls.py
+++ newecom/ecomapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from ecomapp import views
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact/', views.contactus, name='contact'),
+    path('about/', views.aboutus, name='aboutus'),
 ]
